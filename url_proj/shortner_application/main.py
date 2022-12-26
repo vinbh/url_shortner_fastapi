67f5e62e9a3e633a894a27a5d7f73d33d0dc88d7
vinbh <vinayak.farcry@gmail.com>
2022-12-26 16:29:08 +0000

    even odd fastAPi

--- url_proj/shortner_application/main.py
+++ url_proj/shortner_application/main.py
@@ -1,23 +1,19 @@
 from fastapi import FastAPI, HTTPException
-import validators
-import schemas
-
 
 app = FastAPI()
-
 
 def raise_bad_req(message):
     raise HTTPException(status_code=400, detail=message)
 
-@app.get("/")
-def read_root():
-    return "Hello test"
-
-
-@app.post("/url")
-def create_url(url: schemas.URLBase):
-    if not validators.url(url.target_url):
-        raise_bad_req(message="your provided URL isnt valid")
-    return f"todo: make db netry {url.target_url}"    
+@app.get("/check_num/{num}")
+def check_if_evenOdd(num):
+    try:
+        num = int(num)
+    except Exception as e:
+        return "wrong type, integer only"    
+    if num%2 == 0:
+        return f"{num} is even"
+    else:
+         return f"{num} is odd"   
 
 
